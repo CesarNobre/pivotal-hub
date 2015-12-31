313b0873c8a8e2b351e0c84f566df056fb6715de
César Nobre <cesarnobrefilho@gmail.com>
2015-12-30 22:34:16 -0200

    [update] added httparty tool to consuming rest api

--- lib/Story.rb
+++ lib/Story.rb
@@ -1,3 +1,24 @@
-def Story
+require 'httparty'
+class Story
+ include HTTParty
+ 
+ def initialize
+ 	@base_uri = "www.pivotaltracker.com/"
+ 	@project_id = '1187076'
+ 	@project_uri = "#{base_uri}#{project_id}"
+ 	@token = "95dcacc6aebd8ed2a22fe4979685d0de"
+ 	@header = {
+ 		headers => { 
+ 			'X-TrackerToken' => "#{@token}",
+ 			'Content-Type' => 'application/json', 
+ 			'Accept' => 'application/json'}
+ 	}
+
+ end
+
+ def update story_id, options
+ 	options.merge(@header)
+ 	self.class.put("#{@project_uri}/stories/#{story_id}",options)
+ end
 
 end
