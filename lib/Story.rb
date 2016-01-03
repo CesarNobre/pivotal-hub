27d3c1fbd12af303b1ed1c83294a4d64bd64b021
César Nobre <cesarnobrefilho@gmail.com>
2016-01-03 00:25:23 -0200

    [update] change some interactions with user

--- lib/Story.rb
+++ lib/Story.rb
@@ -6,9 +6,14 @@ module Pivotal_Hub
   attr_reader :client, :project, :git
 
   def initialize
+   if File.exists?('.git')
     @git = Git.open(Dir.pwd)
   	@client = TrackerApi::Client.new(token: @git.config('pivotal.api-token'))
   	@project  = client.project(@git.config('pivotal.project-id'))
+   else
+    puts "Ops, you are not in git directory!"
+    exit
+   end
   end
 
   def update_state story_id, state
@@ -19,11 +24,7 @@ module Pivotal_Hub
   end
 
   def get_current_branch
-   if File.exists?('.git')
-     git = Git.open(Dir.pwd)
-     git.current_branch
-   end
-   puts "You are not in git directory!"
+     @git.current_branch
   end
   
   end
